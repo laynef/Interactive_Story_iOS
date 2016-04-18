19818ece53ccb3f485e0a3f6f8d2b68c4d9e1055
Layne Faler <laynefaler@gmail.com>
2016-04-18 13:01:08 -0400

    Initial Commit

---
+++ InteractiveStory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InteractiveStory
+//
+//  Created by Layne Faler on 4/18/16.
+//  Copyright © 2016 Mean Green Machine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
