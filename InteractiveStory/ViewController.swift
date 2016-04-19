830f08bb2111c94815c90175abad19ba9f6a5fd7
Layne Faler <laynefaler@gmail.com>
2016-04-18 21:50:38 -0400

    Interactive story board completed with two functional choices

--- InteractiveStory/ViewController.swift
+++ InteractiveStory/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "startAdventure" {
+            if let pageController = segue.destinationViewController as? PageController {
+                pageController.page = Adventure.story
+            }
+        }
+    }
 }
 
